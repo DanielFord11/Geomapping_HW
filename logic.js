373c8bcb9d6bb8844106af03c178ff0a56cc5924
Daniel Ford <69986354+DanielFord11@users.noreply.github.com>
2021-01-29 16:12:15 -0700

    Update logic.js

--- logic.js
+++ logic.js
@@ -10,7 +10,6 @@ function colorGradient(d) {
                   '#dbf9a6 ';
 }
    
-
 // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
   // Once we get a response, send the data.features object to the createFeatures function
